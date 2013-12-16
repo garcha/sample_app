25f0c05d4a2a00c1ab3173b6bead830f6a6f4e54
blaze2236 <jaspreet@garcha.com>
2013-12-15 21:19:17 -0800

    rspec add

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   
   describe "GET /Home Page" do
     it "should have the content 'sample App'" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -23,7 +25,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    	expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,8 +37,20 @@ describe "StaticPages" do
 
   	it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    	expect(page).to have_title("#{base_title} | About Us")
     end
+  end
+
+  describe "GET /Contant Us" do
+  	it "should have the content 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content("Contact Us")
+  	end
+
+  	it "should have the title 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title("#{base_title} | Contact Us")
+  	end
   end
 
 end

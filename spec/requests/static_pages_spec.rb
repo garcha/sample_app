4a21f1f0b89be9810a54a817fda40a0affab5595
blaze2236 <jaspreet@garcha.com>
2013-12-22 21:22:42 -0800

    update rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "GET /Home Page" do
     before {visit root_path}
 
-    it { should have_content ("Sample App")}
+    it { should have_selector('h1', text:'Sample App') }
     it { should have_title (full_title ("")) }
     it { should_not have_title ('| Home')}
   end
@@ -15,22 +15,45 @@ describe "Static pages" do
   describe "GET /Help Page" do
     before {visit help_path}
 
-   	it { should have_content ("Help")}
+   	it { should have_selector('h1', text:'Help')}
     it { should have_title (full_title ("Help"))}
   end
 
   describe "GET /About Page" do
     before {visit about_path}
 
-  	it {should have_content ("About Us")}
+  	it {should have_selector('h1', text:'About Us')}
   	it { should have_title (full_title ("About Us"))}
   end
 
   describe "GET /Contant Us" do
     before {visit contact_path}
 
-  	it {should have_content ("Contact Us")}
+  	it {should have_selector('h1', text:'Contact Us')}
     it {should have_title (full_title ( "Contact Us"))}
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About Us"
+    expect(page).to have_title (full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title (full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title (full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title (full_title)
+    click_link "Sign up"
+    expect(page).to have_title (full_title('Sign Up'))
+    click_link "Sample app"
+    expect(page).to have_title (full_title)
+  end
 end
+
+
+
+
+
+
+
+

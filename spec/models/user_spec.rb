7e33f8caeb6706be9f9ce73edf95bc3317ac3ef3
blaze2236 <jaspreet@garcha.com>
2013-12-29 22:06:17 -0800

    add user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,51 @@ describe User do
 
   it {should respond_to(:name)}
   it {should respond_to(:email)}
+  it {should be_valid}
+
+  describe "when name is not present" do
+  	before {@user.name = " "}
+  	it {should_not be_valid}
+  end
+
+  describe "when email is not present" do
+  	before {@user.email = " "}
+  	it {should_not be_valid}
+  end
+
+  describe "when username is too long" do
+    before {@user.name = "a" * 51}
+    it {should_not be_valid}
+  end
+
+  describe "When email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "When email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM a_us-ER@f.b.org first.last@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "When email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it {should_not be_valid}
+  end
+
 end

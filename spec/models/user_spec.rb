4a3bc0c43416abb72f8d2ebab8448e949105a7ba
Jaspreet Garcha <jaspreet@garcha.com>
2014-01-09 22:23:48 -0800

    signin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,11 @@ require 'spec_helper'
 
     it {should be_valid}
 
+    describe "remember token" do
+	before {@user.save}
+	its(:remember_token) {should_not be_blank}
+    end
+
     describe "when name is not present" do
     	before {@user.name = " "}
     	it {should_not be_valid}
@@ -93,6 +98,13 @@ require 'spec_helper'
         it { should_not eq user_for_invalid_password }
         specify { expect(user_for_invalid_password).to be_false }
       end
+    end
+
+    describe User do
+
+	it{should respond_to(:password_confirmation)}
+	it{should respond_to(:remember_token)}
+	it{should respont_to(:authenticate)}
     end
 end
 

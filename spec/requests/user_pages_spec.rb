6853bcdca42e65aa04653dfc7abe06ae8ac55240
Jaspreet Garcha <jaspreet@garcha.com>
2014-01-12 12:51:39 -0800

    edit user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -43,7 +43,22 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-  end
+
+    describe "edit" do
+	let(:user) { FactoryGirl.create(:user) }
+	before { visit edit_user_path(user) }
+
+	describe "page" do
+	  it { should have_content("Update your profile") }
+	  it { should have_title("Edit user") }
+  	  it { should have_link('Change', href: 'http://gravatar.com/emails') }
+	end
+
+	describe "with invalid information" do 
+	  before { click_button "Save change" }
+
+	  it { should have_content('error')}
+	end
+      end
+    end	
 end
-
-
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,17 @@ def full_title(page_title)
 		"#{base_title} | #{page_title}"
 	end
 end
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		#sign in when not using capybara.
+		remember_token = User.new_remember_token
+		cookiers[:remember_token] = remember_token
+		user.update_attribute(:remember_toekn, User.encrypt(remember_token))
+	else
+		visit sign_path
+		fill_in "Email", 		with: user.Email
+		fill_in "Password", 	with: user.Password
+		click_button "Sign in"
+	end
+end
